11d887ebf55ce8df3d0ba8c07e2ee5759de51971
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-29 13:44:04 +0000

    Updated generated version

--- packagename/conftest.py
+++ packagename/conftest.py
@@ -4,6 +4,7 @@ import os
 
 from astropy.version import version as astropy_version
 
+# For Astropy 3.0 and later, we can use the standalone pytest plugin
 if astropy_version < '3.0':
     from astropy.tests.pytest_plugins import *  # noqa
     del pytest_report_header
