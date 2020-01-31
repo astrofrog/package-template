30a31afcc0702b5d85b87da7e0a18d28decd30d9
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-31 14:46:37 +0000

    Rerender

--- packagename/conftest.py
+++ packagename/conftest.py
@@ -1,5 +1,8 @@
 # This file is used to configure the behavior of pytest when using the Astropy
-# test infrastructure.
+# test infrastructure. It needs to live inside the package in order for it to
+# get picked up when running the tests inside an interpreter using
+# packagename.test
+
 import os
 
 from astropy.version import version as astropy_version
@@ -8,22 +11,29 @@ from astropy.version import version as astropy_version
 if astropy_version < '3.0':
     from astropy.tests.pytest_plugins import *  # noqa
     del pytest_report_header
+    ASTROPY_HEADER = True
 else:
-    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+    try:
+        from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+        ASTROPY_HEADER = True
+    except ImportError:
+        ASTROPY_HEADER = False
 
 
 def pytest_configure(config):
 
-    config.option.astropy_header = True
+    if ASTROPY_HEADER:
 
-    # Customize the following lines to add/remove entries from the list of
-    # packages for which version numbers are displayed when running the tests.
-    PYTEST_HEADER_MODULES.pop('Pandas', None)
-    PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+        config.option.astropy_header = True
 
-    from .version import version
-    packagename = os.path.basename(os.path.dirname(__file__))
-    TESTED_VERSIONS[packagename] = version
+        # Customize the following lines to add/remove entries from the list of
+        # packages for which version numbers are displayed when running the tests.
+        PYTEST_HEADER_MODULES.pop('Pandas', None)
+        PYTEST_HEADER_MODULES['scikit-image'] = 'skimage'
+
+        from . import __version__
+        packagename = os.path.basename(os.path.dirname(__file__))
+        TESTED_VERSIONS[packagename] = __version__
 
 # Uncomment the last two lines in this block to treat all DeprecationWarnings as
 # exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
